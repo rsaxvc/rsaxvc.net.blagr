2eb7c765aab7916219ef8ff1a7dc6264ded71e42
Richard Allen <rsaxvc@rsaxvc.net>
2015-09-21 20:32:43 -0500

    Build the blog once on launching the daemon
    Dunno why I didn't do this first.
    
    Type: UI Improvement

--- autobuilder.sh
+++ autobuilder.sh
@@ -3,7 +3,7 @@
 command -v inotifywait >/dev/null 2>&1 || { echo >&2 "I require inotifywait but it's not installed.  Aborting."; exit 1; }
 
 while true; do
-	change=$(inotifywait -r -e close_write,moved_to,create input)
 	./build_blog.sh
 	#sensible-browser ../blog/index.html
+	change=$(inotifywait -r -e close_write,moved_to,create input)
 done
